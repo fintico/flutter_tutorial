40cfa045a34d17f5f6ad068cceeb092d02b6df83
Edwin Guerrero <edwin@acchiwan.pe>
2020-04-24 17:34:25 -0500

    Se corrige el bug cuando ya no hay elemento de la lista y se consyriye un mensaje final

--- lib/main.dart
+++ lib/main.dart
@@ -12,46 +12,50 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': ['Red', 'Blue', 'Yellow', 'Green']
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': ['Lion', 'Dog', 'Duck', 'Cat', 'Bear']
+    },
+    {
+      'questionText': 'Who\'s your favorite instructor?',
+      'answers': ['Luis', 'Juan', 'Ana', 'Maria', 'Carla']
+    },
+  ];
   int _questionIndex = 0;
 
   void _myAnswerQuestion() {
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
-    print(_questionIndex);
   }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    const questions = [
-      {
-        'questionText': 'What\'s your favorite color?',
-        'answers': ['Red', 'Blue', 'Yellow', 'Green']
-      },
-      {
-        'questionText': 'What\'s your favorite animal?',
-        'answers': ['Lion', 'Dog', 'Duck', 'Cat', 'Bear']
-      },
-      {
-        'questionText': 'Who\'s your favorite instructor?',
-        'answers': ['Luis', 'Juan', 'Ana', 'Maria', 'Carla']
-      },
-    ];
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(title: Text('Flutter Tutorial')),
-          body: Column(
-            children: <Widget>[
-              Question(
-                questions[_questionIndex]['questionText'],
+        appBar: AppBar(title: Text('Flutter Tutorial')),
+        body: _questionIndex < questions.length
+            ? Column(
+                children: <Widget>[
+                  Question(
+                    questions[_questionIndex]['questionText'],
+                  ),
+                  ...(questions[_questionIndex]['answers'] as List<String>)
+                      .map((answer) {
+                    return Answer(_myAnswerQuestion, answer);
+                  }).toList(),
+                ],
+              )
+            : Center(
+                child: Text('Gracias'),
               ),
-              ...(questions[_questionIndex]['answers'] as List<String>)
-                  .map((answer) {
-                return Answer(_myAnswerQuestion, answer);
-              }).toList(),
-            ],
-          )),
+      ),
     );
   }
 }
